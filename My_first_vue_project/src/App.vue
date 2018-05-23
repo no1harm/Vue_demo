c2c699740df204ffaad23399bf47ae12b4bd16b8
no1harm <chen09869@gmail.com>
2018-05-23 15:46:38 +0800

    完成todolist基本功能(增删修改以及localStore的存取)

--- My_first_vue_project/src/App.vue
+++ My_first_vue_project/src/App.vue
@@ -3,32 +3,48 @@
     <h1 v-text="title"></h1>
     <input type="text" v-model="newItem" v-on:keyup.enter="addNew">
     <ul>
-      <li v-for="item in items" v-bind:class="{finished:item.isFinished}" v-on:click="toggleFinish(item)">
+      <li v-for="item in items" v-bind:class="{finished:item.isFinished}">
+        <input type="checkbox" class="toggle" v-bind:class="{isDone:item.isChecked}" v-on:click="toggleFinish(item)"/>
         {{item.label}}
+        <button v-on:click="deleteItem(item)">删除</button>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import Store from './store' //把store.js当成模块/库/引进当前vue文件
 export default {
-  data:function(){
+  data:function(){  
     return {
       title : "This is a todo list!",
-      items : [
-      ],
-      newItem:'',
+      items : Store.fetch(),  //通过fetch()获取信息
+      newItem:'',  //新输入信息
+    }
+  },
+  watch:{ //观察items的变化
+    items:{
+      handler:function(items){
+        Store.save(items); //存储items到数据库
+      },
+      deep:true,  //深度观察
     }
   },
   methods:{
-    toggleFinish:function(item){
+    toggleFinish:function(item){ //点击打勾CheckBox，添加删除线
         item.isFinished = !item.isFinished;
+        item.isChecked = !item.isChecked;
     },
-    addNew:function(){
+    addNew:function(){  //增加新选项
       this.items.push({
         label:this.newItem,
         isFinished:false,
+        isChecked:false,
       })
+    },
+    deleteItem:function(item){  //删除选项（获取选项下标然后根据下标删除）
+      var index = this.items.indexOf(item);
+      this.items.splice(index,1);
     },
   }
 }
@@ -36,7 +52,7 @@ export default {
 
 <style>
 .finished {
-  text-decoration: underline;
+  text-decoration: line-through ;
 }
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
@@ -55,5 +71,8 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   font-weight: 600;
   font-size: 1.5em;
+}
+#app span {
+  font-size: 0.6em;
 }
 </style>
