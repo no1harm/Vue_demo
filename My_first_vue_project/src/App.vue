ed77ec020f0b70e85f16c25f338d85db2eac8ba2
no1harm <chen09869@gmail.com>
2018-05-23 11:26:41 +0800

    熟悉vue命令

--- My_first_vue_project/src/App.vue
+++ My_first_vue_project/src/App.vue
@@ -1,17 +1,43 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <h1 v-text="title"></h1>
+    <input type="text" v-model="newItem" v-on:keyup.enter="addNew">
+    <ul>
+      <li v-for="item in items" v-bind:class="{finished:item.isFinished}" v-on:click="toggleFinish(item)">
+        {{item.label}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  data:function(){
+    return {
+      title : "This is a todo list!",
+      items : [
+      ],
+      newItem:'',
+    }
+  },
+  methods:{
+    toggleFinish:function(item){
+        item.isFinished = !item.isFinished;
+    },
+    addNew:function(){
+      this.items.push({
+        label:this.newItem,
+        isFinished:false,
+      })
+    },
+  }
 }
 </script>
 
 <style>
+.finished {
+  text-decoration: underline;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -19,5 +45,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#app ul{
+   list-style: none;
+}
+#app li {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  font-weight: 600;
+  font-size: 1.5em;
 }
 </style>
