54a02090d04bb854f29152780f0a5f5c09e1e51c
no1harm <chen09869@gmail.com>
2018-05-24 18:50:48 +0800

    初涉Vue组件

--- My_first_vue_project/src/App.vue
+++ My_first_vue_project/src/App.vue
@@ -9,18 +9,26 @@
         <button v-on:click="deleteItem(item)">删除</button>
       </li>
     </ul>
+    <p> Child tell me this : {{childWord}}</p>
+    <!-- 使用组件componentA -->
+    <componentA msgtext="Here Done" v-on:child-tell-me="listenToMyBoy"></componentA>   
   </div>
 </template>
 
 <script>
 import Store from './store' //把store.js当成模块/库/引进当前vue文件
+import ComponentA from './components/componentA'  //把组件componentA引入当前文件
 export default {
   data:function(){  
     return {
       title : "This is a todo list!",
       items : Store.fetch(),  //通过fetch()获取信息
       newItem:'',  //新输入信息
+      childWord:'',
     }
+  },
+  components:{  //声明组件
+    ComponentA, 
   },
   watch:{ //观察items的变化
     items:{
@@ -31,7 +39,7 @@ export default {
     }
   },
   methods:{
-    toggleFinish:function(item){ //点击打勾CheckBox，添加删除线
+    toggleFinish:function(item){ //点击事件打勾CheckBox，添加删除线
         item.isFinished = !item.isFinished;
         item.isChecked = !item.isChecked;
     },
@@ -45,6 +53,9 @@ export default {
     deleteItem:function(item){  //删除选项（获取选项下标然后根据下标删除）
       var index = this.items.indexOf(item);
       this.items.splice(index,1);
+    },
+    listenToMyBoy:function(msg){
+        this.childWord =msg;
     },
   }
 }
---
+++ My_first_vue_project/src/components/componentA.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="hello">
+        <h1>{{ msg }}</h1>
+        <h1>{{ msgtext}}</h1>
+        <button v-on:click="OnClickMe">Open mouse!</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data:function(){
+        return {
+            msg:"Goodmorning Sir.",
+        }
+    },
+    props:['msgtext'],  //我的理解：相当于创造了一个在父组件和子组件之间可以互相指涉的参数
+    methods:{
+        OnClickMe:function(){
+            // console.log(this.msgtext)
+            this.$emit('child-tell-me',this.msg);   //触发当前实例上的事件。附加参数都会传给监听器回调。
+        }
+    },
+}
+</script>
+
+<style scoped>
+h1 {
+    color: brown;
+}
+</style>
+
+
